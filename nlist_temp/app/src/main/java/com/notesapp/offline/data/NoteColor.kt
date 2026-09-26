eb780136b8a4e97f929b8afc5394d52f181c0abf
amrrady-mentalist <amrrady4020@gmail.com>
2026-09-26 05:59:31 +0300

    Delete nlist_temp directory

--- nlist_temp/app/src/main/java/com/notesapp/offline/data/NoteColor.kt
+++
@@ -1,8 +0,0 @@
-package com.notesapp.offline.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-enum class NoteColor {
-    NONE, VIOLET, ROSE, TEAL, AMBER, SKY, LIME, CORAL
-}
--- nlist_temp/app/src/main/java/com/notesapp/offline/data/ThemeMode.kt
+++
@@ -1,6 +0,0 @@
-package com.notesapp.offline.data
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-enum class ThemeMode { DARK, LIGHT, SYSTEM }
